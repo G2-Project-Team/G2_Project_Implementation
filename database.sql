8ad769af0370bd6ab559e59f24caf222573a54a3
AFELL300 <afell300@caledonian.ac.uk>
2025-10-23 13:47:16 +0100

    Tweaked database structure and worked on login tools

--- database.sql
+++ database.sql
@@ -3,52 +3,40 @@ CREATE DATABASE `websitedb`;
 USE `websitedb`;
 
 CREATE TABLE `Users` (
-  `UserID` int PRIMARY KEY AUTO_INCREMENT,
-  `Username` varchar(255) UNIQUE NOT NULL,
-  `Password` varchar(255) NOT NULL,
-  `Email` varchar(255) UNIQUE NOT NULL,
-  `Type` varchar(255) COMMENT 'Can be ''Land Manager'', ''Developer'', or ''Admin''',
-  `Description` varchar(255),
-  `SavedListingID` int
+  `user_id` int PRIMARY KEY AUTO_INCREMENT,
+  `username` varchar(255) UNIQUE NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `email` varchar(255) UNIQUE NOT NULL,
+  `type` varchar(255) COMMENT 'Can be ''Land Manager'', ''Developer'', or ''Admin''',
+  `description` varchar(255)
 );
 
 CREATE TABLE `GridData` (
-  `GridID` int PRIMARY KEY AUTO_INCREMENT,
-  `CoordinateX` int NOT NULL,
-  `CoordinateY` int NOT NULL,
-  `AvgSunlight` decimal(3,2),
-  `AvgWindSpeed` decimal(3,2),
-  `HasWater` boolean NOT NULL DEFAULT false,
-  `IsFarmland` boolean NOT NULL DEFAULT false,
-  `IsResidential` boolean NOT NULL DEFAULT false
+  `grid_id` int PRIMARY KEY AUTO_INCREMENT,
+  `lat` int NOT NULL,
+  `long` int NOT NULL,
+  `avg_sun` decimal(3,2),
+  `avg_wind` decimal(3,2),
+  `has_water` boolean NOT NULL DEFAULT false,
+  `is_farm` boolean NOT NULL DEFAULT false
 );
 
 CREATE TABLE `LandListings` (
-  `ListingID` int PRIMARY KEY AUTO_INCREMENT,
-  `GridSquareID` int NOT NULL,
-  `TimeCreated` timestamp DEFAULT (now()),
-  `TimeUpdated` timestamp DEFAULT (now()),
-  `UserID` int NOT NULL
+  `listing_id` int PRIMARY KEY AUTO_INCREMENT,
+  `grid_id` int NOT NULL,
+  `time_created` timestamp DEFAULT (now()),
+  `time_updated` timestamp DEFAULT (now()),
+  `user_id` int NOT NULL
 );
 
 CREATE TABLE `Documents` (
-  `DocuID` int PRIMARY KEY AUTO_INCREMENT,
-  `ListingID` int NOT NULL,
-  `DocuLink` varchar(255) NOT NULL
+  `doc_id` int PRIMARY KEY AUTO_INCREMENT,
+  `listing_id` int NOT NULL,
+  `link` varchar(255) NOT NULL
 );
 
-CREATE TABLE `ListingSave` (
-  `SaveID` int PRIMARY KEY AUTO_INCREMENT,
-  `UserID` int NOT NULL,
-  `ListingID` int NOT NULL
+CREATE TABLE `listing_save` (
+  `save_id` int PRIMARY KEY AUTO_INCREMENT,
+  `user_id` int NOT NULL,
+  `listing_id` int NOT NULL
 );
-
-ALTER TABLE `LandListings` ADD FOREIGN KEY (`GridSquareID`) REFERENCES `GridData` (`GridID`);
-
-ALTER TABLE `Documents` ADD FOREIGN KEY (`ListingID`) REFERENCES `LandListings` (`ListingID`);
-
-ALTER TABLE `LandListings` ADD FOREIGN KEY (`UserID`) REFERENCES `Users` (`UserID`);
-
-ALTER TABLE `ListingSave` ADD FOREIGN KEY (`UserID`) REFERENCES `Users` (`UserID`);
-
-ALTER TABLE `ListingSave` ADD FOREIGN KEY (`ListingID`) REFERENCES `LandListings` (`ListingID`);
---
+++ database_old.sql
@@ -0,0 +1,54 @@
+DROP DATABASE IF EXISTS `websitedb`;
+CREATE DATABASE `websitedb`;
+USE `websitedb`;
+
+CREATE TABLE `Users` (
+  `UserID` int PRIMARY KEY AUTO_INCREMENT,
+  `Username` varchar(255) UNIQUE NOT NULL,
+  `Password` varchar(255) NOT NULL,
+  `Email` varchar(255) UNIQUE NOT NULL,
+  `Type` varchar(255) COMMENT 'Can be ''Land Manager'', ''Developer'', or ''Admin''',
+  `Description` varchar(255),
+  `SavedListingID` int
+);
+
+CREATE TABLE `GridData` (
+  `GridID` int PRIMARY KEY AUTO_INCREMENT,
+  `CoordinateX` int NOT NULL,
+  `CoordinateY` int NOT NULL,
+  `AvgSunlight` decimal(3,2),
+  `AvgWindSpeed` decimal(3,2),
+  `HasWater` boolean NOT NULL DEFAULT false,
+  `IsFarmland` boolean NOT NULL DEFAULT false,
+  `IsResidential` boolean NOT NULL DEFAULT false
+);
+
+CREATE TABLE `LandListings` (
+  `ListingID` int PRIMARY KEY AUTO_INCREMENT,
+  `GridSquareID` int NOT NULL,
+  `TimeCreated` timestamp DEFAULT (now()),
+  `TimeUpdated` timestamp DEFAULT (now()),
+  `UserID` int NOT NULL
+);
+
+CREATE TABLE `Documents` (
+  `DocuID` int PRIMARY KEY AUTO_INCREMENT,
+  `ListingID` int NOT NULL,
+  `DocuLink` varchar(255) NOT NULL
+);
+
+CREATE TABLE `ListingSave` (
+  `SaveID` int PRIMARY KEY AUTO_INCREMENT,
+  `UserID` int NOT NULL,
+  `ListingID` int NOT NULL
+);
+
+ALTER TABLE `LandListings` ADD FOREIGN KEY (`GridSquareID`) REFERENCES `GridData` (`GridID`);
+
+ALTER TABLE `Documents` ADD FOREIGN KEY (`ListingID`) REFERENCES `LandListings` (`ListingID`);
+
+ALTER TABLE `LandListings` ADD FOREIGN KEY (`UserID`) REFERENCES `Users` (`UserID`);
+
+ALTER TABLE `ListingSave` ADD FOREIGN KEY (`UserID`) REFERENCES `Users` (`UserID`);
+
+ALTER TABLE `ListingSave` ADD FOREIGN KEY (`ListingID`) REFERENCES `LandListings` (`ListingID`);
