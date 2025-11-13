ae38f055ec1c1f4550faecc74b3acf130aba9e53
AFELL300 <afell300@caledonian.ac.uk>
2025-11-13 12:30:25 +0000

    New template + file structure changes

---
+++ includes/footer.php
@@ -0,0 +1,49 @@
+</body>
+<footer class="mt-3 pt-3 rounded-top" style="background-color:#45A583 !important;border-bottom: 2px solid #45A583;">
+    <div class="container my-3" style="background-color:#45A583 !important">
+        <div class="row">
+            <div class="col order-3">
+                <h5 class="text-black-50"><u>Contact Us</u></h5>
+                <p class="text-black-50">+44 123 123 1234 <br>example@email.com <br>Monday - Friday / 9:00am - 5:00pm</p>
+            </div>
+            <div class="col">
+                <h5 class="text-black-50"><u>Information</u></h5>
+                <p class="text-black-50"><a class="text-reset" href="terms_and_conditions.php" target="_blank">Terms and Conditions</a><br>
+                <a class="text-reset" href="privacy_policy.php" target="_blank">Privacy Policy</a></p>
+            </div>
+            <div class="col">
+            <h5 class="text-black-50"><u>Our Address</u></h5>
+                <p class="text-black-50">[STREET NAME]<br>[CITY NAME]<br>[POSTCODE]</p>
+            </div>
+        </div>
+        <hr>
+        <div class="row">
+            <div class="col">
+                <img src="images\logo.png" style="max-height:70px;">
+            </div>
+            <div class="col">
+                <img src="images\logo.png" style="max-height:70px;">
+            </div>
+            <div class="col">
+                <img src="images\logo.png" style="max-height:70px;">
+            </div
+            <div class="col">
+                <img src="images\logo.png" style="max-height:70px;">
+            </div
+        </div>
+        <hr>
+        <div class="row">
+        <p><small class="text-black-50">&copy2025-2026 [COMPANY NAME]. All rights reserved.</small></p>
+        </div>
+    </div>
+ </footer>
+
+<script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" 
+integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" 
+crossorigin="anonymous"></script>
+    
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js" 
+integrity="sha384-Fy6S3B9q64WdZWQUiU+q4/2Lc9npb8tCaSX9FK7E8HnRr0Jz8D6OP9dO5Vg3Q9ct" 
+crossorigin="anonymous"></script>
+
+</html>
---
+++ template_new.php
@@ -0,0 +1,41 @@
+<?php
+session_start();
+include 'includes/nav.php';
+?>	
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Template</title>
+</head>
+
+
+
+<!-- START OF BODY TAG-->
+
+<body>
+    <div class="container border rounded my-3">
+        <div class="row">
+            <div class="col">
+            <div class="row">
+                    <div class="col">
+                        <h1><u>Welcome!</u></h1>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque sagittis, justo in hendrerit convallis, metus nunc interdum urna, vitae suscipit nulla lorem in elit. Morbi nunc eros, efficitur ut erat et, interdum interdum elit. Donec laoreet, magna sed facilisis bibendum, sem velit vulputate tellus, eget tincidunt neque lorem sed metus. Suspendisse potenti. Etiam neque est, tincidunt ac lorem ac, gravida facilisis urna. Vivamus placerat auctor augue, ac volutpat nisi placerat eget. Pellentesque eget ex est. Nullam sem risus, facilisis et ex a, tristique tempor lorem. Pellentesque lobortis sollicitudin nunc, vitae pellentesque nisl. Fusce blandit justo nibh, at facilisis sem placerat et. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Sed vel molestie nisi, vel pharetra neque. Sed congue aliquam turpis, nec lobortis dui sodales et. </p>
+                    </div>
+                    <div class="col">
+                        <img class="img-fluid my-2" src="images\forest.jpg"></img>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+
+<!-- END OF BODY TAG-->
+
+</html>
+
+<?php
+include 'includes/footer.php';
+?>
